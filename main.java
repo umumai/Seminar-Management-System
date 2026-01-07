fad2a12fdcbfbdefb337742e5eade1fcc155d450
Fatimah Najihah <fatimahnajihah05@gmail.com>
2026-01-07 12:07:06 +0800

    UPDATE : added schedule table into ScheduleFrame

--- main.java
+++ main.java
@@ -2,11 +2,13 @@ import javax.swing.SwingUtilities;
 
 public class main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> { // ensure GUI is created on the EDT 
-            DBHelper.initDatabase(); 
-            LoginFrame lf = new LoginFrame(); // open login frame
-            lf.setVisible(true);
-        });
+    //     SwingUtilities.invokeLater(() -> { // ensure GUI is created on the EDT 
+    //         DBHelper.initDatabase(); 
+    //         LoginFrame lf = new LoginFrame(); // open login frame
+    //         lf.setVisible(true);
+    //     });
+    ScheduleFrame sf = new ScheduleFrame();
+    sf.setVisible(true);
     }
 }
 
