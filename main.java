70c28d6d62e2363c829a4f998c03ffb1563e289e
Fatimah <145191276+fatimahnaj@users.noreply.github.com>
2026-01-07 12:20:03 +0800

    Revert main.java

--- main.java
+++ main.java
@@ -2,13 +2,11 @@ import javax.swing.SwingUtilities;
 
 public class main {
     public static void main(String[] args) {
-    //     SwingUtilities.invokeLater(() -> { // ensure GUI is created on the EDT 
-    //         DBHelper.initDatabase(); 
-    //         LoginFrame lf = new LoginFrame(); // open login frame
-    //         lf.setVisible(true);
-    //     });
-    ScheduleFrame sf = new ScheduleFrame();
-    sf.setVisible(true);
+        SwingUtilities.invokeLater(() -> { // ensure GUI is created on the EDT 
+            DBHelper.initDatabase(); 
+            LoginFrame lf = new LoginFrame(); // open login frame
+            lf.setVisible(true);
+        });
     }
 }
 
